58160f3de27d0e827493293c2ce523173e8dc459
Andy <acameron851@gmail.com>
2019-04-05 09:33:35 +0100

    login and logout visibility working

--- Green_Leaf_Web/index.js
+++ Green_Leaf_Web/index.js
@@ -9,6 +9,44 @@
     };
     firebase.initializeApp(config);
 
+    // Get Elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnLogout = document.getElementById('btnLogout');
+    const divAdmin = document.getElementById('admin_content');
+
+    // Add login event
+    btnLogin.addEventListener('click', e => {
+        // Get email and pass
+        const email = txtEmail.value;
+        const password = txtPassword.value;
+        const auth = firebase.auth();
+        // sign in
+        const promise = auth.signInWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+    });
+
+    // Add logout event
+    btnLogout.addEventListener('click', e => {
+        firebase.auth().signOut();
+        divAdmin.style="visibility: hidden"
+    });
+
+    //Add a realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+            // window.location.href = "index.html";
+            divAdmin.style="visibility: visible"
+        } else {
+            console.log('not logged in');
+            btnLogout.classList.add('hide');
+            // divAdmin.style="visibility: hidden"
+        }
+    });
+
     // Get elements
     const preObject = document.getElementById('object');
     var chartHeading = document.getElementById('weekHeading');
